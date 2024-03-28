28e702a05cbdf0a222398a3124dfc4edb67ddd5c
Beryl Ilenwabor <berylijn@gmail.com>
2024-03-28 10:57:25 +0100

    Connect to database, installed new dependencies.

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB).then(() => {
+    console.log("Connected to MongoDB")
+}).catch((error) => {
+    console.log(error)
+})
 
 const app = express();
 
