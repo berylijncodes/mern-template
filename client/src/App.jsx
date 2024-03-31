a59e09043eb4ab2e9540db2a683e15b086bbc277
Beryl Ilenwabor <berylijn@gmail.com>
2024-03-31 16:40:03 +0200

    Conditionally render Header when path is not sign up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,33 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
-
 import Home from './pages/Home';
-import About from './pages/About'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
+      <AppContent />
+    </BrowserRouter>
+  );
+};
+
+const AppContent = () => {
+  const location = useLocation();
+  const isSignUpPage = location.pathname === '/sign-up';
+
+  return (
+    <>
+      {!isSignUpPage && <Header />}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
       </Routes>
-      
-    </BrowserRouter>
-  )
-}
+    </>
+  );
+};
 
-export default App
+export default App;
