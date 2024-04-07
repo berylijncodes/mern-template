2380cac6dabbd53520379b58a8d8a2684879026f
Beryl Ilenwabor <berylijn@gmail.com>
2024-04-07 12:41:42 +0200

    Complete sign in functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,116 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-  return (
-    <div>
-      Sign In Here!
-    </div>
-  )
-}
+  const[formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-export default SignIn
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+  return (
+    <section className="flex flex-col md:flex-row items-center justify-between h-screen">
+      <div
+        className="h-full w-full md:flex justify-center relative hidden"
+        style={{
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundImage: 'url("assets/images/form-image-3.jpg")',
+          backgroundBlendMode: "darken",
+        }}
+      >
+        <Link href="/">
+          <img
+            src="/logos/logo-dark-outline.svg"
+            alt="logo"
+            width={170}
+            height={50}
+            className="absolute left-8 top-4"
+          />
+        </Link>
+      </div>
+
+      <div className="flex flex-col justify-center items-center w-full h-full md:justify-start md:p-2.5">
+        <div className="max-w-xl w-full p-12 md:backdrop-blur-30 md:bg-opacity-90 sm:p-8 h-full mt-14">
+          <div className="flex items-center gap-4 w-full">
+            <h3 className="md:text-3xl">Sign in</h3>
+            <img
+              src="/assets/icons/hand.png"
+              alt="hand"
+              width={30}
+              height={30}
+            />
+          </div>
+
+          <p className="text-gray-500 mb-2">Please enter your details</p>
+
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <input
+              type="email"
+              placeholder="email"
+              className="border p-3 rounded-lg"
+              id="email"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              className="border p-3 rounded-lg"
+              id="password"
+              onChange={handleChange}
+            />
+            <button
+              disabled={loading}
+              className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+            >
+              {loading ? "Loading..." : "Sign In"}
+            </button>
+          </form>
+          <div className="flex gap-2 mt-5">
+            <p>Dont have an account?</p>
+            <Link to={"/sign-up"}>
+              <span className="text-blue-700">Sign up</span>
+            </Link>
+          </div>
+          {error && <p className="text-red-500 mt-5">{error}</p>}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SignIn;
