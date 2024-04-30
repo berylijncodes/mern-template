a93a96fa92f0be19dd919fb8a7fd6e1b87446a0b
Beryl Ilenwabor <berylijn@gmail.com>
2024-04-30 15:12:47 +0200

    Create API route for user listings

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -50,18 +50,18 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 
-// export const getUserListings = async (req, res, next) => {
-//   if (req.user.id === req.params.id) {
-//     try {
-//       const listings = await Listing.find({ userRef: req.params.id });
-//       res.status(200).json(listings);
-//     } catch (error) {
-//       next(error);
-//     }
-//   } else {
-//     return next(errorHandler(401, "You can only view your own listings!"));
-//   }
-// };
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
+  }
+};
 
 // export const getUser = async (req, res, next) => {
 //   try {
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,6 +1,11 @@
 import express from "express";
 
-import { test, updateUser, deleteUser } from "../controllers/userController.js";
+import {
+  test,
+  updateUser,
+  deleteUser,
+  getUserListings,
+} from "../controllers/userController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -8,5 +13,6 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
